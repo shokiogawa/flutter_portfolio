c91b35b39c4e922634e103aaaba2a05a5046f848
shokiogawa <syoki2578@outlook.jp>
2021-01-23 14:10:54 +0900

    add: プロジェクト一覧を取得して来る。

---
+++ lib/view/components/project_contents.dart
@@ -0,0 +1,73 @@
+import 'package:communitygetandpost/domain/value_object/project.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProjectContents extends StatelessWidget {
+  final Project project;
+
+  ProjectContents(this.project);
+
+  @override
+  Widget build(BuildContext context) {
+    final black = Theme.of(context).textTheme.button.color;
+    final opacityBlack = Theme.of(context).primaryTextTheme.button.color;
+    return Column(
+      children: [
+        Expanded(
+            flex: 3,
+            child: _imageArea(project.imageUrl)),
+        Expanded(flex: 2, child: _textArea(project, black, opacityBlack))
+      ],
+    );
+  }
+
+  Widget _imageArea(String url) {
+    return ConstrainedBox(
+        constraints: BoxConstraints.expand(
+          height: 110,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+          child: Image.network(
+            url,
+            fit: BoxFit.cover,
+          ),
+        ));
+  }
+
+
+
+  Widget _textArea(
+      Project project, Color black, Color opacityBlack) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 2.0, left: 3.0),
+            child: Container(
+              height: 35,
+              child: Text(
+                project.projectName,
+                style: TextStyle(
+                    color: opacityBlack,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w600),
+                overflow: TextOverflow.clip,
+                maxLines: 2,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 1.0),
+            child: Container(
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/my_page.dart
+++ lib/view/my_page.dart
@@ -1,12 +1,21 @@
+import 'dart:io';
+
+import 'package:communitygetandpost/domain/value_object/project.dart';
 import 'package:communitygetandpost/presentation/controller/get_project_controller.dart';
 import 'package:communitygetandpost/presentation/controller/login_page_controller.dart';
 import 'package:communitygetandpost/usecase/read_model/category.dart';
+import 'package:communitygetandpost/view/components/project_contents.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class MyPage extends StatelessWidget {
-  final List<Category> categories = [
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+   List<Category> categories = [
     Category(
         categoryId: 1, categoryName: "おすすめ", categoryEnglish: "recommended"),
     Category(
@@ -15,6 +24,9 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Tab>_buildTabs = categories.map((category) => Tab(text: category.categoryName)).toList();
+    List<Widget> _buildWidget = categories.map((category) => ProjectListWidget(category.categoryId)).toList();
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -81,80 +93,86 @@ class MyPage extends StatelessWidget {
                 floating: true,
                 forceElevated: innerBoxScrolled,
                 bottom: TabBar(
-                  onTap: (value){
-                    Provider.of<GetProjectController>(context, listen: false).getProject();
+                  onTap: (value) {
+                    if(value == 0){
+                      Provider.of<GetProjectController>(context, listen: false)
+                          .getProject();
+                    }
                   },
                   labelColor: Colors.black,
                   indicatorColor: Colors.blueGrey,
                   indicatorSize: TabBarIndicatorSize.tab,
                   tabs: [
-                    Tab(text: categories[0].categoryName),
-                    Tab(text: categories[1].categoryName)
+                    ..._buildTabs,
                   ],
                 ),
               ),
             ];
           },
-          // slivers: [
-          //   SliverAppBar(
-          //     expandedHeight: 100,
-          //     backgroundColor: Colors.white,
-          //     flexibleSpace: Container(
-          //       padding: EdgeInsets.only(top: 20),
-          //       margin: EdgeInsets.symmetric(horizontal: 15),
-          //       child: Column(
-          //         crossAxisAlignment: CrossAxisAlignment.start,
-          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //         children: [
-          //           Row(
-          //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //             children: [
-          //               Text("マイプロジェクト", style: TextStyle(fontSize: 18),),
-          //               InkWell(
-          //                 child: Text(
-          //                   "プロジェクトを作成する。",
-          //                   style: TextStyle(
-          //                       fontSize: 14,
-          //                       fontWeight: FontWeight.w600,
-          //                       color: Colors.blue),
-          //                 ),
-          //                 onTap: () => Navigator.of(context).pushNamed("/new_post_page"),
-          //               )
-          //             ],
-          //           ),
-          //           Container(
-          //             child: Text("あなたのスケジュール", style: TextStyle(fontSize: 20),),
-          //           )
-          //         ],
-          //       ),
-          //     ),
-          //     pinned: true,
-          //     floating: true,
-          //   ),
-          //   SliverList(
-          //     delegate: SliverChildListDelegate([
-          //       Container(
-          //         child: Column(
-          //           children: [
-          //             RaisedButton(
-          //               child: Text("ログアウト"),
-          //               onPressed: () => Provider.of<LoginPageController>(context,
-          //                       listen: false)
-          //                   .logOut()
-          //                   .then((value) => Navigator.of(context)
-          //                       .pushNamedAndRemoveUntil(
-          //                           "/login_page", (route) => false)),
-          //             ),
-          //             Text("HOMEです。"),
-          //           ],
-          //         ),
-          //       ),
-          //     ]),
-          //   )
-          // ],
-          body: TabBarView(children: [Text("おすすめ"), Text("Myプロジェクト")]),
+          body: TabBarView(children: [
+            ..._buildWidget
+          ],),
         ),
       ),
     );
+  }
+}
+
+class ProjectListWidget extends StatefulWidget {
+  final int number;
+  // final Project project;
+
+  ProjectListWidget(this.number);
+
+  @override
+  _ProjectListWidgetState createState() => _ProjectListWidgetState();
+}
+
+class _ProjectListWidgetState extends State<ProjectListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    print(widget.number.toString());
+    final allProject = Provider.of<GetProjectState>(context, listen: true).allProjects;
+    return CustomScrollView(
+      key: PageStorageKey(widget.number),
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.all(10.0),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2,
+              childAspectRatio: 0.75,
+            ),
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return ProjectCard(allProject[index]);
+            },
+            childCount: allProject.length,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ProjectCard extends StatelessWidget {
+  final Project project;
+  ProjectCard(this.project);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+      },
+      child: Card(
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Stack(children: <Widget>[
+            ProjectContents(project),
+            // ...item.tags.map((tag) => Tag(TagTheme.RED, tag)).toList(),
+          ])),
+    );;
   }
 }
