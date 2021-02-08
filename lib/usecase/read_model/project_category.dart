aa026873097863b9213ba70585bb074b588eec90
shokiogawa <syoki2578@outlook.jp>
2021-02-08 11:19:54 +0900

    add: カテゴリー選択可能1

---
+++ lib/usecase/read_model/project_category.dart
@@ -0,0 +1,19 @@
+class ProjectCategory{
+  final int id;
+  final String projectCategoryName;
+
+  ProjectCategory({this.id, this.projectCategoryName});
+}
+
+final List<ProjectCategory> projectCategory =[
+  ProjectCategory(id: 0, projectCategoryName: "テクノロジー"),
+  ProjectCategory(id: 1, projectCategoryName: "アウトドア"),
+  ProjectCategory(id: 2, projectCategoryName: "健康"),
+  ProjectCategory(id: 3, projectCategoryName: "スポーツ"),
+  ProjectCategory(id: 4, projectCategoryName: "勉強"),
+  ProjectCategory(id: 5, projectCategoryName: "外国語"),
+  ProjectCategory(id: 6, projectCategoryName: "ボランティア"),
+  ProjectCategory(id: 7, projectCategoryName: "ゲーム"),
+  ProjectCategory(id: 8, projectCategoryName: "プログラミング"),
+  ProjectCategory(id: 9, projectCategoryName: "ビジネス"),
+];
