cb0f6aca8ec0b02eaa05b2624a0b2c0462a2a8a8
shokiogawa <syoki2578@outlook.jp>
2021-02-13 15:59:21 +0900

    add:searchdelegate導入。

---
+++ lib/view/components/project_search_delegate.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:communitygetandpost/domain/value_object/project.dart';
+
+class ProjectSearchDelegate extends SearchDelegate<Project> {
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildResults(context);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return _buildResults(context);
+  }
+
+  Widget _buildResults(BuildContext context) {
+    return Container();
+  }
+}
